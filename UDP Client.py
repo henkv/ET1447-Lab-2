95eca66757ed3deae64bd77c34adc7b48167afd6
Henrik Vik <henkvik@gmail.com>
2018-05-23 13:43:08 +0200

    Update UDP Client.py

--- UDP Client.py
+++ UDP Client.py
@@ -1,16 +1,23 @@
 from socket import *
 from time import time
+from sys import argv
 
-targetHost = "1.1.1.2"
+
+duration = int(argv[1])
+package_per_sec = int(argv[2])
+targetHost = argv[3]
 targetPort = 12000
+
+print("Duration: "    + argv[1] + ", "
+    + "Packages/s: "  + argv[2] + ", "
+    + "Target: "      + argv[3] + "\n")
 
 client = socket(AF_INET, SOCK_DGRAM)
 counter = 0
 payload = ";" + "".zfill(94)
 
-packageDelay = 1/50
+packageDelay = 1/package_per_sec
 
-duration = 15
 now = time()
 endTime = now + duration
 nextPackageTime = now + packageDelay
